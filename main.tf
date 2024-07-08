cb35dfef6cb8e3f578b6aaab1b446dbd0f2b075f
mahiparu <mahiparu4567@gmail.com>
2024-07-08 14:48:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,20 @@ resource "aws_instance" "ec2_example" {
         Name = "Terraform EC2"
     }
 }
+
+
+
+///
+resource "aws_instance" "instance"{
+      ami = "ami-04629cfb3bd2d73f3"        
+      instance_type = "t2.micro"
+      security_groups = ["default"]
+      key_name = "ravi_y"
+      tags = {
+            Name = "Web-server-by-TerraForm"
+      }
+}
+output "my-public-ip"{
+       value= aws_instance.instance.public_ip
+}
+///
